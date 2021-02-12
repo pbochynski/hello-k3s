983861d24828f3b681c84a6db96d19243ba80643
pbochynski <p.bochynski@gmail.com>
2021-02-12 21:34:37 +0100

    Cache for docker build

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,3 +1,6 @@
 export VERSION=$(cat app/package.json| jq -r '.version')
-docker build -f Dockerfile -t registry.localhost:5000/pbochynski/hello-k3s:$VERSION app
+docker build -f Dockerfile -t registry.localhost:5000/pbochynski/hello-k3s:$VERSION \
+  --cache-from ghcr.io/pbochynski/hello-k3s:0.1.1 \
+  --build-arg BUILDKIT_INLINE_CACHE=1 \
+  app 
 docker push registry.localhost:5000/pbochynski/hello-k3s:$VERSION
